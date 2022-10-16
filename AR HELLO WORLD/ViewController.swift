cb66ba6cfdddfc1c83a9715fe240fb3651608dc9
Dark-Sniper101 <rylanberry@icloud.com>
2022-10-16 15:09:26 -0500

    Initial Commit

---
+++ AR HELLO WORLD/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AR HELLO WORLD
+//
+//  Created by Rylan Berry on 10/16/22.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
